ab4274ee8775e1ec3590b1723be6881c3699c59c
Sumit Singh <49089114+sumit1311singh@users.noreply.github.com>
2026-09-01 19:51:40 -0400

    Step 8: apply_standardizer

--- model.py
+++ model.py
@@ -77,8 +77,10 @@ def fit_standardizer(X):
 
     return x_mean, x_std
 
-# Step 8 - apply_standardizer (not yet solved)
-# TODO: implement
+# Step 8 - apply_standardizer
+def apply_standardizer(X, mean, std):
+    # TODO: Return the scaled matrix (X - mean) / std via broadcasting.
+    return (X-mean)/std
 
 # Step 9 - add_bias_column (not yet solved)
 # TODO: implement
